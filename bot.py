f324a9933e4d538a6d414fd559d5064e69753ebf
Dagon <dag@gmail.com>
2026-08-23 10:31:26 +0300

    Объеденил две колонки date и time в одну. Теперь используется timectamptz для определения точного времени(Только Московского)

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command, CommandStart, CommandObject
-from sql import isUserExist, delTask, changeTask, changeDate, addAll, addTgIdandName, addUserId
+from sql import isUserExist, delTask, changeTask, changeDate, addAll, addTgIdandName, watch
+
+from datetime import datetime, time
+import pytz
+
+from keyboards import getChange, getMenu, getSome
 
 from dotenv import load_dotenv
 import os
@@ -26,9 +31,20 @@ load_dotenv()
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 dp = Dispatcher()
 
+
+
 @dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(text=f'Привет, {message.from_user.first_name}, я бот напоминалка. Моя задача принимать твои задачи и время, в которое ты хочешь их выполнить.',
+                         reply_markup=getMenu())
+
+
+
+
+
+@dp.message(F.text == 'Создать задачу')
 async def start(message: Message, state: FSMContext):
-    await message.answer(text=f'Привет, {message.from_user.first_name}, я бот напоминалка. Моя задача принимать твои задачи и время, в которое ты хочешь их выполнить. Для того чтобы начать напишите что угодно')
+    await message.answer('Для того чтобы начать напишите что угодно')
     name = message.from_user.first_name
     tg_id = message.from_user.id
     if isUserExist(tg_id) == "Пользователь есть":
@@ -39,8 +55,7 @@ async def start(message: Message, state: FSMContext):
         addTgIdandName(name, tg_id)
         await state.update_data(taskId=tg_id)
         await state.set_state(Dialog.setId)
-
-    
+            
 @dp.message(Dialog.setId)
 async def what_should_i_make(message: Message, state: FSMContext):
     await message.answer('Теперь можете ввести то, что вы хотите сделать в будущем')
@@ -55,9 +70,7 @@ async def time_and_date(message: Message, state: FSMContext):
         await message.answer("Название запомнил, теперь введите дату дедлайна в формате DD.MM.YYYY")
         await state.update_data(taskName=task_text)
         await state.set_state(Dialog.setDate)
-
-    
-        
+       
 @dp.message(Dialog.setDate)
 async def startCreate(message: Message, state: FSMContext):
     list = []
@@ -71,8 +84,7 @@ async def startCreate(message: Message, state: FSMContext):
             
     elif list[2] != '.' and list[5] != '.':
         await message.answer("Дата введена не правильно, чтобы продолжить начните заново, нажав на /start")
-    
-        
+       
     #Проверка, если месяц февраль и в нём введён день больше 28, то будет выводится ошибка
     elif ((list[3] + list[4] != '01') or (list[3] + list[4] !='03') or (list[3] + list[4] !='05') or (list[3] + list[4] !='07') or (list[3] + list[4] !='08') or (list[3] + list[4] !='10') or (list[3] + list[4] !='12') or (list[3] + list[4] !='04') or (list[3] + list[4] !='06') or (list[3] + list[4] !='09') or (list[3] + list[4] !='11')) and (int(list[0]) >= 2 and int(list[1]) > 8):
         await message.answer("Дата введена не правильно, чтобы продолжить начните заново, нажав на /start")
@@ -85,20 +97,14 @@ async def startCreate(message: Message, state: FSMContext):
     elif ((int(list[0]) >= 3 and int(list[1]) >= 1) and ((list[3] + list[4] != '01') or (list[3] + list[4] !='03') or (list[3] + list[4] !='05') or (list[3] + list[4] !='07') or (list[3] + list[4] !='08') or (list[3] + list[4] !='10') or (list[3] + list[4] !='12') or (list[3] + list[4] !='02'))):
         await message.answer("Дата введена не правильно, чтобы продолжить начните заново, нажав на /start")
 
-    
     #Проверка, если определённые суммированный объекты выводят год меньше или равно 2025, то будет выдоваться ошибка    
     elif int(list[6] + list[7] + list[8] + list[9]) <= 2025:
         await message.answer("Дата введена не правильно, чтобы продолжить начните заново, нажав на /start")
-
-
        
     else:
         await state.update_data(taskDate=data_text)
         await state.set_state(Dialog.setTime)
         await message.answer("Дату запомнил, теперь введите время дедлайна в формате HH:MM")
-
-
-
 
 @dp.message(Dialog.setTime)
 async def startCreate(message: Message, state: FSMContext):
@@ -106,7 +112,6 @@ async def startCreate(message: Message, state: FSMContext):
     list = []
     data = await state.get_data()
 
-    
     for i in time_text:
         list.append(i)
         
@@ -125,13 +130,11 @@ async def startCreate(message: Message, state: FSMContext):
         time_text = message.text
         await state.update_data(taskTime=time_text)
 
-        await message.answer(f"Итого задача:\n {data["taskName"]} которую нужно сделать до {data["taskDate"]} {time_text}")
+        await message.answer(f"Итого задача:\n {data["taskName"]} которую нужно сделать {data["taskDate"]} в {time_text}")
         await message.answer("Напишите любое сообщение, чтобы подтвердить задачу")
         
         await state.set_state(Dialog.confirm)
     
-
-
 @dp.message(Dialog.confirm)
 async def startCreate(message: Message, state: FSMContext):
     await message.answer("Задача поставлена!")
@@ -139,15 +142,88 @@ async def startCreate(message: Message, state: FSMContext):
     task = data.get('taskName')
     date = data.get('taskDate')                                                 
     time = data.get('taskTime')  
-    addAll(task, time, date)                                               
-    tg_id = data.get('taskId')                                                                                                      
-    addUserId(tg_id, task)                                                  
+    tg_id = data.get('taskId')   
+    
+    user_tz_str = 'Europe/Moscow'
+    user_tz = pytz.timezone(user_tz_str)
+    
+    native_dt = datetime(int(date[6] + date[7] + date[8] + date[9]), int(date[3] + date[4]), int(date[0] + date[1]), int(time[0] + time[1]), int(time[3] + time[4]))
+    aware_dt = user_tz.localize(native_dt)
+    utc_dt = aware_dt.astimezone(pytz.UTC)
+    
+    addAll(task, utc_dt, tg_id)                                                                                                                                                                                                   
     # Сохранение всего в бд                                                 
+
+
+
+
                                                     
-                                                            
+@dp.message(F.text == 'Просмотреть имеющиеся задачи')
+async def watchTask(message: Message, state: FSMContext):
+    data = await state.get_data() 
+    tg_id = data.get('taskId')
+    print(watch(tg_id))
+
+    
+    await message.answer('Ваши задачи:',
+                             reply_markup=getChange()) 
+    
+    
+# @dp.message(F.text.isdigit())
+# async def some(message: Message):
+#     await message.answer("g", 
+#                          reply_markup=getSome(int(message.text)))
+                                                        
                                                             
 async def main():                                                   
-    await dp.start_polling(bot)                                                 
+    await dp.start_polling(bot)  
+    
+   
+    
+    
+# import datetime
+# import os.path
+
+# from google.auth.transport.requests import Request
+# from google.oauth2.credentials import Credentials
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from googleapiclient.discovery import build
+# from googleapiclient.errors import HttpError
+
+# # If modifying these scopes, delete the file token.json.
+# SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+
+# def main():
+#   """Shows basic usage of the Google Calendar API.
+#   Prints the start and name of the next 10 events on the user's calendar.
+#   """
+#   creds = None
+#   # The file token.json stores the user's access and refresh tokens, and is
+#   # created automatically when the authorization flow completes for the first
+#   # time.
+#   if os.path.exists("token.json"):
+#     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+#   # If there are no (valid) credentials available, let the user log in.
+#   if not creds or not creds.valid:
+#     if creds and creds.expired and creds.refresh_token:
+#       creds.refresh(Request())
+#     else:
+#       flow = InstalledAppFlow.from_client_secrets_file(
+#           "credentials.json", SCOPES
+#       )
+#       creds = flow.run_local_server(port=0)
+#     # Save the credentials for the next run
+#     with open("token.json", "w") as token:
+#       token.write(creds.to_json())
+
+# if __name__ == "__main__":
+#   main()
+  
+  
+  
+  
+                                                 
                                                     
 asyncio.run(main())                                                 
                                                     
--- sql.py
+++ sql.py
@@ -37,17 +37,30 @@ def changeDate(change_date):
         cursor.execute("""UPDATE to_do_list SET date_ = %s""", (change_date,))
         a.commit()    
         
-def addAll(task, time, date):
-    with a.cursor() as cursor:
-        cursor.execute("""INSERT INTO to_do_list (task, time_, date_) VALUES(%s, %s, %s)""", (task, time, date))
-        a.commit()
-        
-def addUserId(tg_id, task):
+def addAll(task, date_and_time, tg_id):
+    with b.cursor() as cursor:
+            cursor.execute("""SELECT user_id FROM user_ WHERE tg_id = %s""", (tg_id,))
+            user_id_t = cursor.fetchone()
+            if user_id_t != None:
+                user_id_f = user_id_t[0]
+                with a.cursor() as cursor2:
+                    cursor2.execute("""INSERT INTO to_do_list (user_id, task, date_and_time) VALUES(%s, %s, %s)""", (user_id_f, task, date_and_time))
+                    a.commit()
+                
+def watch(tg_id):
     with b.cursor() as cursor:
         cursor.execute("""SELECT user_id FROM user_ WHERE tg_id = %s""", (tg_id,))
-        user_id_t = cursor.fetchone()
-        if user_id_t != None:
-            user_id_f = user_id_t[0]
+        user_id = cursor.fetchone()
+        
+        if user_id: 
             with a.cursor() as cursor2:
-                cursor2.execute("""UPDATE to_do_list SET user_id = %s WHERE task = %s""", (user_id_f, task))
-                a.commit()
+                cursor2.execute("""SELECT task, time_, date_ FROM to_do_list WHERE user_id = %s""", (user_id,))
+                result = cursor2.fetchall()
+                task = []
+                for i in result:
+                    task.append({
+                        'task': f"{i[0]}", 
+                        'time': f"{i[1]}", 
+                        'date': f"{i[2]}"
+                    })
+                return task
